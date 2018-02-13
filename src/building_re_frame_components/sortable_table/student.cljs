2df6cf18232b9a16d57045efd98ce51ccfb04165
André de Jager <andrethehunter@gmail.com>
2018-02-13 18:19:15 +1000

    WIP :fire: [skip ci]

--- src/building_re_frame_components/sortable_table/student.cljs
+++ src/building_re_frame_components/sortable_table/student.cljs
@@ -1,7 +1,6 @@
 (ns building-re-frame-components.sortable-table.student
   (:require [reagent.core :as reagent]
             [re-frame.core :as rf]))
-
 (def data
   [["Name" "Weapon" "Side" "Height (m)"]
    ["Luke Skywalker" "Blaster" "Good" 1.72]
@@ -9,18 +8,54 @@
    ["Han Solo" "Blaster" "Good" 1.8]
    ["Obi-Wan Kenobi" "Light Saber" "Good" 1.82]
    ["Chewbacca" "Bowcaster" "Good" 2.28]
-   ["Darth Vader" "Light Saber" "Bad" 2.03]
-   ])
+   ["Darth Vader" "Light Saber" "Bad" 2.03]])
+
 
 (rf/reg-event-db
   :initialize
   (fn [db _]
-    {}))
+    (assoc db :tables {:new-hope {:header (first data)
+                                  :rows   (rest data)}})))
+
+(rf/reg-sub
+  :table
+  (fn [db [_ key]]
+    (get-in db [:tables key])))
+
+(defn sortable-table [table]
+  (let [s (reagent/atom [])]
+    (fn [table]
+      (let [table @table
+            [key dir :as sorts] @s
+            rows (cond->> (:rows table)
+                          key (sort-by #(nth % key))
+                          (= :descending dir) reverse)]
+        [:table
+         [:tr
+          (for [[i h] (map vector (range) (:header table))]
+            [:th
+             {:on-click #(condp = sorts
+                           [i :ascending] (reset! s [])
+                           [i :descending] (reset! s [i :ascending])
+                           (reset! s [i :descending]))
+              :style    {:cursor :pointer}}
+             [:div
+              h
+              (condp = sorts
+                [i :ascending] "▲"
+                [i :descending] "▼"
+                nil)]])]
+         (for [row rows]
+           [:tr
+            (for [v row]
+              [:td v])])]))))
 
 (defn ui []
-  [:div
-   "Put table here"])
+  (let [table (rf/subscribe [:table :new-hope])]
+    (fn []
+      [sortable-table table])))
 
 (when-some [el (js/document.getElementById "sortable-table--student")]
   (defonce _init (rf/dispatch-sync [:initialize]))
   (reagent/render [ui] el))
+
