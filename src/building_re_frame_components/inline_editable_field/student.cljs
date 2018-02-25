accf9104fbf196529dba82f5964d78028f80956f
André de Jager <andrethehunter@gmail.com>
2018-02-26 08:57:52 +1000

    Completed inline editable field

--- src/building_re_frame_components/inline_editable_field/student.cljs
+++ src/building_re_frame_components/inline_editable_field/student.cljs
@@ -14,12 +14,41 @@
   (fn [db _]
     (:movies db)))
 
+(rf/reg-event-db
+  :update-movie
+  (fn [db [_ id kw val]]
+    (assoc-in db [:movies id kw] val)))
+
+(defn inline-editor [txt on-change]
+  (reagent/with-let [s (reagent/atom {:text txt})
+                     toggle-edit #(do
+                                    (.preventDefault %)
+                                    (swap! s update :editing? not))]
+    (if (:editing? @s)
+      [:form {:on-submit #(do
+                            (when on-change
+                              (on-change (:text @s)))
+                            (toggle-edit %))}
+       [:textarea {:value     (:text @s)
+                   :on-change #(swap! s assoc :text (-> % .-target .-value))}]
+       [:button "Save"]
+       [:button {:on-click #(do
+                              (toggle-edit %)
+                              (swap! s assoc :text txt))}
+        "Cancel"]]
+      [:span
+       {:on-click toggle-edit}
+       txt [:sup "✎"]])))
+
 (defn ui []
-  [:div
-   (for [[movie-id movie] @(rf/subscribe [:movies])]
-     [:div {:key movie-id}
-      [:h3 (:title movie)]
-      [:div (:description movie)]])])
+  (reagent/with-let [sub (rf/subscribe [:movies])]
+    [:div
+     (for [[movie-id {:keys [:title :description]}] @sub]
+       [:div {:key movie-id}
+        [:h3 [inline-editor title
+              #(rf/dispatch [:update-movie movie-id :title %])]]
+        [:div [inline-editor description
+               #(rf/dispatch [:update-movie movie-id :description %])]]])]))
 
 (when-some [el (js/document.getElementById "inline-editable-field--student")]
   (defonce _init (rf/dispatch-sync [:initialize]))
