ae0cd62016403460629cfc1d923e7e86cd1b8770
André de Jager <andrethehunter@gmail.com>
2018-04-13 17:24:48 +1000

    Completed accordion_component

--- src/building_re_frame_components/accordion_component/student.cljs
+++ src/building_re_frame_components/accordion_component/student.cljs
@@ -1,22 +1,40 @@
 (ns building-re-frame-components.accordion-component.student
-  (:require [reagent.core :as reagent]
-            [re-frame.core :as rf]))
+  (:require
+    [reagent.core :as reagent]))
 
-(rf/reg-event-db
-  :initialize
-  (fn [_ _]
-    {}))
+(defn accordion [{:keys [:active :children]}]
+  (let [state (reagent/atom {:current active})]
+    (fn [{:keys [:active :children]}]
+      (let [i-h-c (map-indexed cons (partition-all 2 children))]
+        [:div
+         (doall
+           (for [[i header content] i-h-c]
+             ^{:key i}
+             [:div
+              [:div {:style    {:background-color "grey" :color "white"}
+                     :on-click (fn []
+                                 (swap! state update :current
+                                   #(if (= i %) nil i)))}
+               header]
+              [:div
+               {:style {:background-color "lightgrey"
+                        :height           (if (= i (:current @state))
+                                            (get-in @state [:refs i])
+                                            0)
+                        :overflow         :hidden
+                        :transition       "height 0.2s"}}
+               [:div
+                {:ref #(swap! state assoc-in [:refs i]
+                         (when %
+                           (.-clientHeight %)))}
+                content]]]))]))))
 
 (defn ui []
-  [:div
-   [:div
-    "a"
-    [:p "Choice A"]
-    "b"
-    [:p "Choice B"]
-    "c"
-    [:p "Choice C"]]])
+  [accordion
+   {:active   1
+    :children ["a" [:p "Choice A"]
+               "b" [:p "Choice B"]
+               "c" [:p "Choice C"]]}])
 
 (when-some [el (js/document.getElementById "accordion-component--student")]
-  (defonce _init (rf/dispatch-sync [:initialize]))
   (reagent/render [ui] el))
